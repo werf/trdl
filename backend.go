e1ae401a579f2525ab7c23fe8a07f79a15f97586
Alexey Igrychev <alexey.igrychev@flant.com>
2021-04-16 16:16:02 +0100

    Installable plugin

--- backend.go
+++ backend.go
@@ -2,21 +2,14 @@ package trdl
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-func Factory(ctx context.Context, c *logical.BackendConfig) (logical.Backend, error) {
-	b := backend()
-	if err := b.Setup(ctx, c); err != nil {
-		return nil, err
-	}
-	return b, nil
-}
-
-type trdlBackend struct {
+type backend struct {
 	*framework.Backend
 
 	l sync.RWMutex
@@ -25,13 +18,32 @@ type trdlBackend struct {
 	providerCtxCancel context.CancelFunc
 }
 
-func backend() *trdlBackend {
-	b := new(trdlBackend)
+var _ logical.Factory = Factory
+
+func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend, error) {
+	b, err := newBackend()
+	if err != nil {
+		return nil, err
+	}
+
+	if conf == nil {
+		return nil, fmt.Errorf("configuration passed into backend is nil")
+	}
+
+	if err := b.Setup(ctx, conf); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func newBackend() (*backend, error) {
+	b := new(backend)
 	b.providerCtx, b.providerCtxCancel = context.WithCancel(context.Background())
 
 	b.Backend = &framework.Backend{
-		BackendType: logical.TypeUnknown,
 		Help:        backendHelp,
+		BackendType: logical.TypeLogical,
 		Paths: framework.PathAppend(
 			[]*framework.Path{
 				pathRelease(b),
@@ -39,7 +51,7 @@ func backend() *trdlBackend {
 		),
 	}
 
-	return b
+	return b, nil
 }
 
 const (
