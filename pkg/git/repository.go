3e4b10204fe7cfb483b0b6bf9e714e5b465188a8
Alexey Igrychev <alexey.igrychev@flant.com>
2021-04-28 16:43:50 +0100

    Refactoring

--- pkg/git/repository.go
+++ pkg/git/repository.go
@@ -1,10 +1,12 @@
 package git
 
 import (
+	"archive/tar"
 	"fmt"
 	"io"
 	"os"
 	"path"
+	"time"
 
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
@@ -14,6 +16,8 @@ import (
 
 type CloneOptions struct {
 	TagName           string
+	BranchName        string
+	ReferenceName     string
 	RecurseSubmodules git.SubmoduleRescursivity
 }
 
@@ -27,6 +31,10 @@ func CloneInMemory(url string, opts CloneOptions) (*git.Repository, error) {
 
 		if opts.TagName != "" {
 			cloneOptions.ReferenceName = plumbing.ReferenceName(fmt.Sprintf("refs/tags/%s", opts.TagName))
+		} else if opts.BranchName != "" {
+			cloneOptions.ReferenceName = plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", opts.BranchName))
+		} else if opts.ReferenceName != "" {
+			cloneOptions.ReferenceName = plumbing.ReferenceName(opts.ReferenceName)
 		}
 
 		if opts.RecurseSubmodules != 0 {
@@ -35,6 +43,39 @@ func CloneInMemory(url string, opts CloneOptions) (*git.Repository, error) {
 	}
 
 	return git.Clone(storage, fs, cloneOptions)
+}
+
+func AddWorktreeFilesToTar(tw *tar.Writer, gitRepo *git.Repository) error {
+	return ForEachWorktreeFile(gitRepo, func(path, link string, fileReader io.Reader, info os.FileInfo) error {
+		size := info.Size()
+
+		// The size field is the size of the file in bytes; linked files are archived with this field specified as zero
+		if link != "" {
+			size = 0
+		}
+
+		if err := tw.WriteHeader(&tar.Header{
+			Format:     tar.FormatGNU,
+			Name:       path,
+			Linkname:   link,
+			Size:       size,
+			Mode:       int64(info.Mode()),
+			ModTime:    time.Now(),
+			AccessTime: time.Now(),
+			ChangeTime: time.Now(),
+		}); err != nil {
+			return fmt.Errorf("unable to write tar entry %q header: %s", path, err)
+		}
+
+		if link == "" {
+			_, err := io.Copy(tw, fileReader)
+			if err != nil {
+				return fmt.Errorf("unable to write tar entry %q data: %s", path, err)
+			}
+		}
+
+		return nil
+	})
 }
 
 func ForEachWorktreeFile(gitRepo *git.Repository, fileFunc func(path string, link string, fileReader io.Reader, info os.FileInfo) error) error {
