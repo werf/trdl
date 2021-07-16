6d2a77375b3b54b913462c1c652366020a0d74b9
Alexey Igrychev <alexey.igrychev@flant.com>
2021-07-16 12:10:11 +0100

    refactor

--- pkg/publisher/periodic.go
+++ pkg/publisher/periodic.go
@@ -3,13 +3,8 @@ package publisher
 import (
 	"context"
 
-	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
-
-func (m *Publisher) Paths() []*framework.Path {
-	return nil
-}
 
 func (m *Publisher) PeriodicFunc(_ context.Context, _ *logical.Request) error {
 	m.mu.Lock()
