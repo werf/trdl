c3bcd4db42ab4bb1ff04c05ef4bb41b7490cebb8
Timofey Kirillov <timofey.kirillov@flant.com>
2021-09-16 11:39:23 +0300

    infra: fix release build script for trdl
    Use bin dir in trdl release structure.

--- client/scripts/ci/build_release_v2.sh
+++ client/scripts/ci/build_release_v2.sh
@@ -9,7 +9,7 @@ if [ -z "$VERSION" ] ; then
 fi
 
 gox -osarch="linux/amd64 linux/arm64 darwin/amd64 darwin/arm64 windows/amd64" \
-    -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/trdl" \
+    -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/bin/trdl" \
     -tags="dfrunmount dfssh" \
     -ldflags="-s -w -X github.com/werf/trdl/client/pkg/trdl.Version=$VERSION" \
         github.com/werf/trdl/client/cmd/trdl
