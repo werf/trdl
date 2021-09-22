6b797cf728c0b1b765a9172998902c91ef33842d
Artem Kosulin <artem.kosulin@flant.com>
2021-09-22 18:36:53 +0300

    Add new index page

---
+++ docs/js/slider.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  const sel = $('.slider__nav');
+  const nav = $('.slider__navigation');
+
+  $('.slider__wrap').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    dots: true,
+    speed: 300,
+    dotsClass: 'slider__dots',
+    appendArrows: sel,
+    appendDots: sel,
+    prevArrow: `<button type="button" class="slider__prev"><svg class="slider__arrow"><use xlink:href="/images/icons/sprite.svg#arrow-left"></use></svg></button>`,
+    nextArrow: `<button type="button" class="slider__next"><svg class="slider__arrow"><use xlink:href="/images/icons/sprite.svg#arrow-left"></use></svg></button>`
+  });
+
+  $('.slider__wrapper').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    dots: true,
+    speed: 300,
+    dotsClass: 'slider__dots',
+    appendArrows: nav,
+    appendDots: nav,
+    prevArrow: `<button type="button" class="slider__prev"><svg class="slider__arrow"><use xlink:href="/images/icons/sprite.svg#arrow-left"></use></svg></button>`,
+    nextArrow: `<button type="button" class="slider__next"><svg class="slider__arrow"><use xlink:href="/images/icons/sprite.svg#arrow-left"></use></svg></button>`
+  });
+});
