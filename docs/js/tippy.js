473250a45bd82bdf0e8f5fb79b9358f1e4adce2f
Anton Klimov <anton.klimov@flant.ru>
2021-09-24 12:47:38 +0300

    updates & fixes

--- docs/js/tippy.js
+++ docs/js/tippy.js
@@ -11,13 +11,13 @@ window.addEventListener('DOMContentLoaded', ()=> {
     interactive: true,
     interactiveDebounce: 75,
     allowHTML: true,
-    content: "TUF (The Update Framework) — фреймворк, который применяется для защиты систем обновления ПО. TUF-репозиторий — любое хранилище (например, S3), для работы с которым используются инструменты безопасности TUF. Подробнее о TUF: <a href='https://theupdateframework.io/'>https://theupdateframework.io/</a>"
+    content: "TUF (The Update Framework) — фреймворк, который применяется для защиты систем обновления ПО. TUF-репозиторий — любое хранилище (например, S3), для работы с которым используются инструменты безопасности TUF. <a href='https://theupdateframework.io/'>Подробнее о TUF</a>."
   });
 
   tippy('#tuf-repo__en', {
     interactive: true,
     interactiveDebounce: 75,
     allowHTML: true,
-    content: "TUF (The Update Framework) is a framework for securing software update systems. TUF repository is any repository with your software (e.g., S3) that uses TUF security tools. Learn more about TUF: <a href='https://theupdateframework.io/'>https://theupdateframework.io/</a>"
+    content: "TUF (The Update Framework) is a framework for securing software update systems. TUF repository is any repository with your software (e.g., S3) that uses TUF security tools. <a href='https://theupdateframework.io/'>Learn more about TUF</a>."
   });
 });
