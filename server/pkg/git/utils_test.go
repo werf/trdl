6c50b6bd24cd1aaf91a618233c252c05eb21022f
Timofey Kirillov <timofey.kirillov@flant.com>
2022-04-06 18:00:46 +0300

    fix(signatures): fix signatures ignored when too many git notes exists for a repo
    Changed git-notes reading algorithm to take into account git notes files fanout naming.
    
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

---
+++ server/pkg/git/utils_test.go
@@ -0,0 +1,36 @@
+package git
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Utils", func() {
+	It("objectFanoutPaths", func() {
+		path := "44344dae578b8c9f53617f9dffec40b3f2ad91ae"
+		expectedRes := []string{
+			"44344dae578b8c9f53617f9dffec40b3f2ad91ae",
+			"44/344dae578b8c9f53617f9dffec40b3f2ad91ae",
+			"44/34/4dae578b8c9f53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae578b8c9f53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/578b8c9f53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b8c9f53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c9f53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/617f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9dffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ffec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec/40b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec/40/b3f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec/40/b3/f2ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec/40/b3/f2/ad91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec/40/b3/f2/ad/91ae",
+			"44/34/4d/ae/57/8b/8c/9f/53/61/7f/9d/ff/ec/40/b3/f2/ad/91/ae",
+		}
+
+		Expect(objectFanoutPaths(path)).To(Equal(expectedRes))
+	})
+})
