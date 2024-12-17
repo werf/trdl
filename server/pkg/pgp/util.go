a7adf685c2270e7256594ff2a9fca2385aa3af6c
Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>
2024-12-17 15:30:23 +0300

    fix
    Signed-off-by: Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>

--- server/pkg/pgp/util.go
+++ server/pkg/pgp/util.go
@@ -14,11 +14,6 @@ func VerifyPGPSignatures(pgpSignatures []string, signedReaderFunc func() (io.Rea
 		return pgpKeys, 0, nil
 	}
 
-	signedReader, err := signedReaderFunc()
-	if err != nil {
-		return nil, 0, err
-	}
-
 	verifiedKeys := make([]string, 0, len(pgpKeys))
 	for _, pgpKey := range pgpKeys {
 		keyring, err := openpgp.ReadArmoredKeyRing(strings.NewReader(pgpKey))
@@ -26,21 +21,31 @@ func VerifyPGPSignatures(pgpSignatures []string, signedReaderFunc func() (io.Rea
 			return nil, 0, err
 		}
 
+		verified := false
+		signedReader, err := signedReaderFunc()
+		if err != nil {
+			return nil, 0, err
+		}
 		for _, pgpSignature := range pgpSignatures {
 			_, err := openpgp.CheckArmoredDetachedSignature(keyring, signedReader, strings.NewReader(pgpSignature))
 			if err != nil {
 				if logger != nil {
-					logger.Debug(fmt.Sprintf("[DEBUG-SIGNATURES] VerifyPGPSignatures -- will skip pgpKey due to error: %s\n>%v<", err, pgpKey))
+					logger.Debug(fmt.Sprintf("[DEBUG-SIGNATURES] Signature verification failed: %s\n>%v<", err, pgpKey))
 				}
 				continue
 			}
+			verified = true
+			break
+		}
+
+		if verified {
 			requiredNumberOfVerifiedSignatures--
+			verifiedKeys = append(verifiedKeys, pgpKey)
 			if requiredNumberOfVerifiedSignatures == 0 {
 				return verifiedKeys, 0, nil
 			}
-			verifiedKeys = append(verifiedKeys, pgpKey)
-			break
 		}
 	}
+
 	return verifiedKeys, requiredNumberOfVerifiedSignatures, nil
 }
