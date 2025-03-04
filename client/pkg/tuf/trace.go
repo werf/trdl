d33f4e4d14c3a193b2c2f3ffe5af61dbe8f79e2c
Aleksei Igrychev <aleksei.igrychev@palark.com>
2025-03-04 13:42:20 +0000

    misly
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- client/pkg/tuf/trace.go
+++ client/pkg/tuf/trace.go
@@ -1,9 +1,27 @@
 package tuf
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"net/http/httptrace"
+	"strings"
+	"time"
+)
+
+const (
+	eventStart              = "START"
+	eventDNSStart           = "DNS_START"
+	eventDNSDone            = "DNS_DONE"
+	eventConnectStart       = "CONNECT_START"
+	eventConnectDone        = "CONNECT_DONE"
+	eventConnectError       = "CONNECT_ERROR"
+	eventTLSStart           = "TLS_START"
+	eventTLSDone            = "TLS_DONE"
+	eventTLSError           = "TLS_ERROR"
+	eventResponseFirstByte  = "RESPONSE_FIRST_BYTE"
+	eventRequestError       = "REQUEST_ERROR"
+	eventRequestDone        = "REQUEST_DONE"
 )
 
 type TracingTransport struct {
@@ -11,22 +29,65 @@ type TracingTransport struct {
 }
 
 func (t *TracingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	prefix := "[TRACE tuf client]"
-	fmt.Printf("%s Making request to: %s\n", prefix, req.URL.String())
+	startTime := time.Now()
+
+	logBase := func(indent int, event, format string, args ...interface{}) {
+		indentStr := strings.Repeat(" ", 2 * indent)
+		fmt.Printf("%s [%.2fs] %s: %s\n",
+			indentStr, time.Since(startTime).Seconds(), event, fmt.Sprintf(format, args...))
+	}
+	
+	logMain := func(event, format string, args ...interface{}) {
+		logBase(1, event, format, args...)
+	}
+	
+	logStep := func(event, format string, args ...interface{}) {
+		logBase(2, event, format, args...)
+	}
+
+	logMain(eventStart, "Request to %s %s", req.Method, req.URL.String())
+
 	trace := &httptrace.ClientTrace{
+		DNSStart: func(info httptrace.DNSStartInfo) {
+			logStep(eventDNSStart, "Lookup %s", info.Host)
+		},
 		DNSDone: func(info httptrace.DNSDoneInfo) {
-			fmt.Printf("%s Finished DNS lookup: %v\n", prefix, info.Addrs)
+			logStep(eventDNSDone, "Resolved %+v", info.Addrs)
+		},
+		ConnectStart: func(network, addr string) {
+			logStep(eventConnectStart, "Connecting to %s (%s)", addr, network)
 		},
 		ConnectDone: func(network, addr string, err error) {
 			if err != nil {
-				fmt.Printf("%s Connection failed: %s over %s, error: %v\n", prefix, addr, network, err)
+				logStep(eventConnectError, "Failed to %s: %v", addr, err)
 			} else {
-				fmt.Printf("%s Connected to %s over %s\n", prefix, addr, network)
+				logStep(eventConnectDone, "Connected to %s", addr)
 			}
+		},
+		TLSHandshakeStart: func() {
+			logStep(eventTLSStart, "Starting handshake")
+		},
+		TLSHandshakeDone: func(state tls.ConnectionState, err error) {
+			if err != nil {
+				logStep(eventTLSError, "Handshake failed: %v", err)
+			} else {
+				logStep(eventTLSDone, "Handshake completed")
+			}
+		},
+		GotFirstResponseByte: func() {
+			logStep(eventResponseFirstByte, "Received first byte")
 		},
 	}
 
 	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 
-	return t.Transport.RoundTrip(req)
+	resp, err := t.Transport.RoundTrip(req)
+	if err != nil {
+		logMain(eventRequestError, "Request failed: %v", err)
+		return nil, err
+	}
+
+	logMain(eventRequestDone, "Completed with status %s (Total: %.2fs)", resp.Status, time.Since(startTime).Seconds())
+	
+	return resp, nil
 }
