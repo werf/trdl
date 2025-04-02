701eda54887d45289136766e7af1bb4f4aee5a8e
Evgeniy Frolov <evgeniy.frolov@flant.com>
2025-04-02 10:45:28 +0300

    chore(release): refactor commands use local flags instead of global
    Signed-off-by: Evgeniy Frolov <evgeniy.frolov@flant.com>

--- release/cmd/trdl-vault/main.go
+++ release/cmd/trdl-vault/main.go
@@ -1,98 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	"github.com/werf/trdl/release/common"
-	"github.com/werf/trdl/release/pkg/client"
+	"github.com/werf/trdl/release/cmd/trdl-vault/commands"
 )
 
 func main() {
-	var commonCmdData common.CmdData
-
 	cmd := &cobra.Command{
 		Use:   "trdl-vault",
 		Short: "Trdl CLI for Vault operations",
 	}
 
-	publishCmd := &cobra.Command{
-		Use:   "publish <project-name>",
-		Short: "Publish operation",
-		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			projectName := args[0]
-			if err := publishCmd(&commonCmdData, projectName); err != nil {
-				log.Fatalf("Publish failed: %s", err.Error())
-			}
-		},
-	}
-
-	releaseCmd := &cobra.Command{
-		Use:   "release <project-name> <git-tag>",
-		Short: "Release operation",
-		Args:  cobra.MinimumNArgs(2),
-		Run: func(cmd *cobra.Command, args []string) {
-			projectName := args[0]
-			gitTag := args[1]
-			if err := releaseCmd(&commonCmdData, projectName, gitTag); err != nil {
-				log.Fatalf("Release failed: %s", err.Error())
-			}
-		},
-	}
-
-	common.SetupVaultAddress(&commonCmdData, cmd)
-	common.SetupVaultToken(&commonCmdData, cmd)
-	common.SetupRetry(&commonCmdData, cmd)
-	common.SetupMaxAttemps(&commonCmdData, cmd)
-	common.SetupDelay(&commonCmdData, cmd)
-	common.SetupLogLevel(&commonCmdData, cmd)
-
-	cmd.AddCommand(publishCmd)
-	cmd.AddCommand(releaseCmd)
-
+	cmd.AddCommand(commands.CreateCommands()...)
 	cmd.SilenceUsage = true
 
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func publishCmd(c *common.CmdData, projectName string) error {
-	trdlClient, err := client.NewTrdlVaultClient(client.NewTrdlVaultClientOpts{
-		VaultAddress: *c.VaultAddress,
-		VaultToken:   *c.VaultToken,
-		Retry:        *c.Retry,
-		MaxAttempts:  *c.MaxAttempts,
-		Delay:        *c.Delay,
-		LogLevel:     c.GetLogLevel(),
-	})
-	if err != nil {
-		return fmt.Errorf("unable to create client: %w", err)
-	}
-	if err := trdlClient.Publish(projectName); err != nil {
-		return fmt.Errorf("unable to publish project: %w", err)
-	}
-	return nil
-}
-
-func releaseCmd(c *common.CmdData, projectName, gitTag string) error {
-	trdlClient, err := client.NewTrdlVaultClient(client.NewTrdlVaultClientOpts{
-		VaultAddress: *c.VaultAddress,
-		VaultToken:   *c.VaultToken,
-		Retry:        *c.Retry,
-		MaxAttempts:  *c.MaxAttempts,
-		Delay:        *c.Delay,
-		LogLevel:     c.GetLogLevel(),
-	})
-	if err != nil {
-		return fmt.Errorf("unable to create client: %w", err)
-	}
-	if err := trdlClient.Release(projectName, gitTag); err != nil {
-		return fmt.Errorf("unable to release project: %w", err)
-	}
-	return nil
 }
--- release/common/common.go
+++ release/common/common.go
@@ -1,9 +1,11 @@
 package common
 
 import (
+	"context"
 	"log/slog"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -11,53 +13,114 @@ import (
 	"github.com/werf/trdl/release/pkg/logger"
 )
 
+type CmdDataKey struct{}
+
 func SetupVaultAddress(cmdData *CmdData, cmd *cobra.Command) {
 	cmdData.VaultAddress = new(string)
-	defaultValue := "http://localhost:8200"
-	envValue := os.Getenv("VAULT_ADDR")
-	if envValue != "" {
-		defaultValue = envValue
-	}
+	defaultValue := getEnvOrDefault("TRDL_VAULT_ADDR", "http://localhost:8200")
 
 	_, err := url.ParseRequestURI(defaultValue)
 	if err != nil {
 		panic(err)
 	}
 
-	cmd.PersistentFlags().StringVarP(cmdData.VaultAddress, "vault-address", "", defaultValue, "Set vault address")
+	cmd.Flags().StringVarP(cmdData.VaultAddress, "vault-address", "", defaultValue, "Set vault address (env: TRDL_VAULT_ADDR)")
 }
 
 func SetupVaultToken(cmdData *CmdData, cmd *cobra.Command) {
 	cmdData.VaultToken = new(string)
-	defaultValue := "root"
-	envValue := os.Getenv("VAULT_TOKEN")
-	if envValue != "" {
-		defaultValue = envValue
-	}
+	defaultValue := getEnvOrDefault("TRDL_VAULT_TOKEN", "root")
 
-	cmd.PersistentFlags().StringVarP(cmdData.VaultToken, "vault-token", "", defaultValue, "Set vault token")
+	cmd.Flags().StringVarP(cmdData.VaultToken, "vault-token", "", defaultValue, "Set vault token (env: TRDL_VAULT_TOKEN)")
 }
 
 func SetupRetry(cmdData *CmdData, cmd *cobra.Command) {
 	cmdData.Retry = new(bool)
-	cmd.PersistentFlags().BoolVarP(cmdData.Retry, "retry", "", true, "Set flag to enable/disable retries")
+	defaultValue := getBoolFromEnv("TRDL_VAULT_RETRY", true)
+	cmd.Flags().BoolVarP(cmdData.Retry, "retry", "", defaultValue, "Enable/disable retries (env: TRDL_VAULT_RETRY)")
 }
 
 func SetupMaxAttemps(cmdData *CmdData, cmd *cobra.Command) {
 	cmdData.MaxAttempts = new(int)
-	cmd.PersistentFlags().IntVarP(cmdData.MaxAttempts, "max-attempts", "", 5, "Set max retries")
+	defaultValue := getIntFromEnv("TRDL_VAULT_MAX_ATTEMPTS", 5)
+	cmd.Flags().IntVarP(cmdData.MaxAttempts, "max-attempts", "", defaultValue, "Set max retries (env: TRDL_VAULT_MAX_ATTEMPTS)")
 }
 
 func SetupDelay(cmdData *CmdData, cmd *cobra.Command) {
 	cmdData.Delay = new(time.Duration)
-	cmd.PersistentFlags().DurationVarP(cmdData.Delay, "delay", "", 10*time.Second, "Set max delay between retries")
+	defaultValue := getDurationFromEnv("TRDL_VAULT_DELAY", 10*time.Second)
+	cmd.Flags().DurationVarP(cmdData.Delay, "delay", "", defaultValue, "Set delay between retries (env: TRDL_VAULT_DELAY)")
 }
 
 func SetupLogLevel(cmdData *CmdData, cmd *cobra.Command) {
 	cmdData.LogLevel = new(string)
-	cmd.PersistentFlags().StringVarP(cmdData.LogLevel, "log-level", "", "", "Set log level (debug, info, warn, error)")
+	defaultValue := getEnvOrDefault("TRDL_VAULT_LOG_LEVEL", "info")
+	cmd.Flags().StringVarP(cmdData.LogLevel, "log-level", "", defaultValue, "Set log level (debug, info, warn, error) (env: TRDL_VAULT_LOG_LEVEL)")
 }
 
 func (c *CmdData) GetLogLevel() slog.Level {
 	return logger.ParseLogLevel(*c.LogLevel)
+}
+
+func WithCmdData(ctx context.Context, cmdData *CmdData) context.Context {
+	return context.WithValue(ctx, CmdDataKey{}, cmdData)
+}
+
+func FromCmdData(ctx context.Context) *CmdData {
+	if v, ok := ctx.Value(CmdDataKey{}).(*CmdData); ok {
+		return v
+	}
+	return nil
+}
+
+func SetupCmdData(cmdData *CmdData, cmd *cobra.Command) {
+	SetupVaultAddress(cmdData, cmd)
+	SetupVaultToken(cmdData, cmd)
+	SetupRetry(cmdData, cmd)
+	SetupMaxAttemps(cmdData, cmd)
+	SetupDelay(cmdData, cmd)
+	SetupLogLevel(cmdData, cmd)
+}
+
+func getEnvOrDefault(envVar string, defaultValue string) string {
+	if val, exists := os.LookupEnv(envVar); exists {
+		return val
+	}
+	return defaultValue
+}
+
+func getBoolFromEnv(envVar string, defaultValue bool) bool {
+	val, exists := os.LookupEnv(envVar)
+	if !exists {
+		return defaultValue
+	}
+	parsed, err := strconv.ParseBool(val)
+	if err != nil {
+		return defaultValue
+	}
+	return parsed
+}
+
+func getIntFromEnv(envVar string, defaultValue int) int {
+	val, exists := os.LookupEnv(envVar)
+	if !exists {
+		return defaultValue
+	}
+	parsed, err := strconv.Atoi(val)
+	if err != nil {
+		return defaultValue
+	}
+	return parsed
+}
+
+func getDurationFromEnv(envVar string, defaultValue time.Duration) time.Duration {
+	val, exists := os.LookupEnv(envVar)
+	if !exists {
+		return defaultValue
+	}
+	parsed, err := time.ParseDuration(val)
+	if err != nil {
+		return defaultValue
+	}
+	return parsed
 }
