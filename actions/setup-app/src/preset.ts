3b4368be95f082129478ba6905e7e5ad5b7081a6
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-04-10 18:52:55 +0300

    test(actions): add more presets
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- actions/setup-app/src/preset.ts
+++ actions/setup-app/src/preset.ts
@@ -4,7 +4,8 @@ import { AddArgs, UpdateArgs } from '../../lib/trdl-cli'
 export enum preset {
   unknown = 'unknown',
   werf = 'werf',
-  kubedog = 'kubedog'
+  kubedog = 'kubedog',
+  nelm = 'nelm'
 }
 
 const cmdAddArgsMap: Record<preset, AddArgs> = {
@@ -27,6 +28,13 @@ const cmdAddArgsMap: Record<preset, AddArgs> = {
     rootVersion: '12',
     rootSha512:
       '6462a80292eb6d7712d8a18126366511f9c47a566f121a7745cfd68b624dc340b6591c2cadfe20690eb38296c399a3f4e6948aca90be60e446ed05c3c238294c'
+  },
+  [preset.nelm]: {
+    repo: preset.nelm,
+    url: 'https://storage.googleapis.com/nelm-tuf',
+    rootVersion: '1',
+    rootSha512:
+      '2122fb476c48de4609fe6d3636759645996088ff6796857fc23ba4b8331a6e3a58fc40f1714c31bda64c709ef6f49bcc4691d091bad6cb1b9a631d8e06e1f308'
   }
 }
 
@@ -44,6 +52,11 @@ const cmdUpdateArgsMap: Record<preset, UpdateArgs> = {
   [preset.kubedog]: {
     repo: preset.kubedog,
     group: '0',
+    channel: 'stable'
+  },
+  [preset.nelm]: {
+    repo: preset.nelm,
+    group: '1',
     channel: 'stable'
   }
 }
